f7e48bce4cddeb5e71ddb821dfd6bb25291dbff8
Bryan Mishkin <bryan.mishkin@gmail.com>
2014-09-17 20:13:12 -0700

    Added a timer that begins counting when the board is loaded and stops counting when the board is solved.

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// Timer variables.
+var sec = 0;
+var timer = null;
+
+$(document).ready(function() {
+	$("#divSudoku").on("boardLoaded", boardLoaded);
+	$("#divSudoku").on("boardSolved", boardSolved);
+});
+
+boardLoaded = function(){
+	// Start the timer.
+	function pad(n) { return (n > 9) ? n : "0" + n; }
+	timer = setInterval(function() {
+	    $("#seconds").html(pad(++sec % 60));
+	    $("#minutes").html(pad(parseInt(sec / 60, 10) % 60));
+	}, 1000);
+}
+
+boardSolved = function(){
+	// Stop the timer.
+	clearInterval(timer);
+}
